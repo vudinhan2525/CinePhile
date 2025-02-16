ca4279132ffd93f26bcbe2b686045b42abfebb1c
vudinhan2525 <vudinhan000@gmail.com>
2025-02-16 13:05:49 +0700

    init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <div className="text-3xl text-red-500">sadasd</div>
+  </StrictMode>
+);
